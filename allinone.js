02ad5f1479a5a1b7d9878cfe2fa202563b5b6a5f
hameermehra <hameermehra@gmail.com>
2022-05-03 21:03:26 +0530

    new

---
+++ allinone.js
@@ -0,0 +1,104 @@
+/*alert("welcome to the earth")
+document.write("how are you")
+
+//oprator..... pre and post increment 
+var a= 10
+var b=20
+var c=a+b
+var d=++a
+console.log(d) 
+// asssinment oprator..
+var a= 10
+var b=20
+var c=a+b
+var d=a===b
+console.log(d)
+
+
+//condtion system
+var a= 10;
+var b=20;
+if(a>b) {
+    console.log("not availble")
+}
+else {
+    console.log("acceptable")
+}
+
+
+//switch cindtion ::   
+var a= 10;
+var b=20;
+switch(20){
+case a : console.log("its a 10")
+break;
+case b :console.log("its 20")
+break;
+
+
+//loop value reapt unless he end
+var count=5;
+while(count<10){
+document.write("hameer mehra");
+count++;
+
+
+
+console.log (`var x = "Rahul" + " " + "sen";`)
+
+a=10
+if(a===10){
+    alert("hi")
+}
+console.log("a")
+
+let str ="please locate where 'locate' occurs!";
+console.log(str.lastIndexOf("locate"))
+
+{
+return (option?"yes":"no");
+}
+bool ans=true;
+console.log(output(ans));
+
+var x=5;
+var y= 2;
+var z=x%y;
+console.log (z=x%y
+
+var check = 0
+while (check<11)
+{console.log(check);   ++check  }
+//outpur 0 to 11
+
+
+var m=7
+m = m++ --m;
+console.log(m);
+
+
+let a = Math.pow(0, 1);
+let b = Math.pow(1, 1);
+let c = Math.pow(1, 10);
+let d = Math.pow(3, 3);
+let e = Math.pow(-3, 3);
+let f = Math.pow(2, 4);
+
+*/function oddTriangularNumber(N)
+{
+    return (N * ((2 * N) - 1));
+}
+ 
+// Driver code
+let N = 3;
+document.write(oddTriangularNumber(N));
+
+
+
+
+
+
+
+
+
+
